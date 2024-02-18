ef9bfdb640381498cbd60f4187b6f9a6b8aaa5c2
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2024-02-18 13:33:06 +0100

    fix: wrong var

--- src/molecule/run_molecule.bash
+++ src/molecule/run_molecule.bash
@@ -7,7 +7,7 @@ GREEN='\033[0;32m'
 find . -name molecule.yml -print0 | 
     while IFS= read -r -d '' line; do
         dir_role=$(dirname "${line}")
-		echo -e "| ${GREEN}Test ${roledir}${NOCOLOR}"
+		echo -e "| ${GREEN}Test ${dir_role}${NOCOLOR}"
         pushd "${dir_role}/../../" || exit 2
         molecule test
         popd || exit 2
