926575b95b7b9a7ce0091ff1783269387a845660
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2024-02-18 12:01:37 +0100

    fix: molecule

--- src/molecule/run_molecule.bash
+++ src/molecule/run_molecule.bash
@@ -2,68 +2,15 @@
 set -e
 
 NOCOLOR='\033[0m'
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-function run_test()
-{
-	local directory="${1}"
-	local type="${2}"
-	
-	if [ ! -d "${directory}" ];
-	then
-		echo -e "| ${RED}Dossier ${directory} non présent${NOCOLOR}"
-		exit 1
-	fi
-	
-	for roledir in "${directory}"/*/molecule; do
-		echo -e "| ${GREEN}Test ${type} ${roledir}${NOCOLOR}"
-	    pushd "$(dirname "${roledir}")" || exit 2
-	    molecule test
-	    popd || exit 2
-	done
-}
-
-function Help()
-{
-   # Display Help
-   echo "Execution des tests molecule du dossier courant."
-   echo
-   echo "Syntax: run_molecule [-h|-r|-p]"
-   echo "options:"
-   echo "-h  Affichage de cette aide."
-   echo "-r  Analyse des rôles."
-   echo "-p  Analyse des playbooks."
-   echo
-}
-
-for arg in "$@" ; do
-   shift
-   case "$arg" in
-      -h)
-		Help
-		exit
-		;;
-      -r) # Analyse des rôles
-         target="roles"
-         message="ROLE"
-         ;;
-      -p) # Analyse des playbooks
-         target="tests"
-         message="PLAYBOOK"
-         ;;
-      *)
-         echo "Error: Invalid option"
-         exit 2
-         ;;
-   esac
-done
-
-
-# Identification du dossier de la collection
-dir_role="$(find . -name galaxy.yml -exec dirname {} \;)"
-cd "${dir_role}" || exit 2
-
-run_test "${target}" "${message}"
+find . -name molecule.yml -print0 | 
+    while IFS= read -r -d '' line; do
+        dir_role=$(dirname "${line}")
+		echo -e "| ${GREEN}Test ${roledir}${NOCOLOR}"
+        pushd "${dir_role}/../../" || exit 2
+        molecule list
+        popd || exit 2
+    done
 
 exit 0
