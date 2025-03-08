ae24512cb884b8f6ab2b0af4ed7d20a884abac45
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2025-03-08 17:28:04 +0100

    molecule: use scenario

--- src/molecule/run_molecule.bash
+++ src/molecule/run_molecule.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
-
+export ANSIBLE_COLLECTIONS_PATH="~/.ansible/collections/:./collections"
 NOCOLOR='\033[0m'
 GREEN='\033[0;32m'
 
 find . -name molecule.yml -print0 | 
     while IFS= read -r -d '' line; do
         dir_role=$(dirname "${line}")
+        scenario=$(echo "${dir_role}" | awk -F '/' '{print $NF}')
 		echo -e "| ${GREEN}Test ${dir_role}${NOCOLOR}"
         pushd "${dir_role}/../../" || exit 2
-        molecule test
+        molecule test -s "${scenario}"
         popd || exit 2
     done
 
