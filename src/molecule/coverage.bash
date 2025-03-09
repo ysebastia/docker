f869558a0591fcf3461b12bab393756445995a77
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2025-03-09 18:51:47 +0100

    fix: use coverage script with new molecule tree

--- src/molecule/coverage.bash
+++ src/molecule/coverage.bash
@@ -10,17 +10,19 @@ for arg in "$@" ; do
    shift
    case "$arg" in
       -h)
-		Help
-		exit
-		;;
+        Help
+        exit
+        ;;
       -r) # Analyse des rôles
          source="roles"
-         molecule="roles"
+         begin_with='name: "'
+         end_with='"$'
          type="d"
          ;;
       -p) # Analyse des playbooks
          source="playbooks"
-         molecule="tests/playbooks"
+         begin_with='.import_playbook: '
+         end_with='$'
          type="f"
          ;;
       *)
@@ -38,35 +40,39 @@ function fake_coverage()
     echo "Collection ${dir_collection}"
     
     nb_source=$(find "${dir_collection}/${source}/" -maxdepth 1 -type "${type}" | grep -vc "/$")
-    nb_molecule=$(find "${dir_collection}/${molecule}/" -name molecule.yml -type f | wc -l)    
+    nb_molecule=0    
     
-    if [ "${nb_source}" -gt 0 ]; then
+
+    for i in $(find "${dir_collection}/${source}/" -maxdepth 1 -type "${type}" | grep -v "/$" | sort);
+    do
+        item="$(echo "${i}" | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')"
+        fqcn="${begin_with}$(echo ${dir_collection} | rev | awk -F '/' '{print $1"."$2}' | rev).${item}${end_with}"
+        nb_call="$(grep -rlE "${fqcn}" "${dir_collection}/extensions/molecule" | wc -l)"
+        if [[ "${nb_call}" -gt 0 ]]; then
+            echo -e "${GREEN}Test ${source}/${item} : OK (${nb_call})${NOCOLOR}"
+            nb_molecule=$(expr $nb_molecule + 1)
+        else
+            echo -e "${RED}Test ${source}/${item} : KO${NOCOLOR}"
+        fi;
+    done;
+    
+    if [[ "${nb_source}" -gt 0 ]]; then
         coverage=$(echo "scale=0;$nb_molecule * 100 / $nb_source" | bc)
     else
         coverage=0
     fi
     sum_coverage=$(echo "$sum_coverage + $coverage" | bc)
 
-    for i in $(find "${dir_collection}/${source}/" -maxdepth 1 -type "${type}" | grep -v "/$" | sort);
-    do
-        item="$(echo "${i}" | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}')"
-        if [ -d "${dir_collection}/${molecule}/${item}" ] && [ "$(find "${dir_collection}/${molecule}/${item}" -name molecule.yml -type f | wc -l)" -eq 1 ]; then
-            echo -e "${GREEN}Test ${source}/${item} : OK${NOCOLOR}"
-        else
-            echo -e "${RED}Test ${source}/${item} : KO${NOCOLOR}"
-        fi;
-    done;
-    
     echo "Collection coverage: ${coverage}" 
     echo ""
 }
 
 while IFS= read -r -d '' line; do 
     dir_collection=$(dirname "${line}")
-    if [ -d "${dir_collection}/${molecule}/" ]; then
+    if [[ -d "${dir_collection}/extensions/molecule/" ]]; then
         fake_coverage
     else
-        echo -e "${ORANGE}Missing directory ${dir_collection}/${molecule}${NOCOLOR}"
+        echo -e "${ORANGE}Missing directory ${dir_collection}/extensions/molecule${NOCOLOR}"
     fi    
 done < <(find . -name galaxy.yml -print0)
 
